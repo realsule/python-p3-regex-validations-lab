88dd27d4d504353ca97d36d77cf6236ee52604c7
Suleiman <real1sule@gmail.com>
2025-11-28 10:57:39 +0300

    complete regex-validations

--- lib/regex.py
+++ lib/regex.py
@@ -11,3 +11,14 @@ phone_regex = re.compile(phone_number)
 
 email_address = r""
 email_regex = re.compile(email_address)
+
+
+name_regex = re.compile(r"^[A-Z][a-zA-Z'-]*(?: [A-Z][a-zA-Z'-]*)*$")
+
+phone_regex = re.compile(
+    r"^(?:\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$"
+)
+
+email_regex = re.compile(
+    r"^[A-Za-z][A-Za-z0-9\.]*@[A-Za-z]+\.[A-Za-z]+$"
+)
